44e44cc95ad9cbcfd39adf1e90f9a10e11ac1d67
omid-faghihi <fhomid3@gmail.com>
2025-05-13 15:31:19 +0330

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { TonConnectButton } from "@tonconnect/ui-react";
+
+function App() {
+  return (
+    <div className="App">
+      <TonConnectButton />
+    </div>
+  );
+}
+
+export default App;
